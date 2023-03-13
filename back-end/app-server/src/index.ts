debf0145dc18044c7aafe53cec59f20a8a2b22cb
Lohan <lohanamendola18@gmail.com>
2023-03-13 09:54:47 -0300

    criando nosso servidor e rotas

---
+++ back-end/app-server/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import statusRoute from "./routes/status.route";
+
+//Configuração padrao
+const app = express();
+const host = 'http://localhost';
+const port = 3000;
+
+//Configurações da Aplicação
+app.use(express.json());
+
+//Configuração das rotas
+app.use(statusRoute);
+
+//Iniciar o servidor 
+app.listen(port, () =>{
+    console.log(`Servidor online: ${host}:${port}`)
+})
